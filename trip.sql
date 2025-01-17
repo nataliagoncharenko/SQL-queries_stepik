27c5b599ddd2ac6249f5146f554647fba4108131
nataliagoncharenko <mjiaahf@gmail.com>
2025-01-17 05:29:36 +0500

    Добавлен последний запрос

--- trip.sql
+++ trip.sql
@@ -59,4 +59,13 @@ FROM trip
 WHERE MONTH(date_first) = 2 OR MONTH(date_first) = 3
 ORDER BY name ASC, Сумма DESC
 
+--10. SELECT name, SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS "Сумма" 
+FROM trip 
+WHERE name IN (SELECT name FROM trip
+               GROUP BY name
+               HAVING COUNT(name) > 3)
+GROUP BY name
+ORDER BY Сумма DESC
+
+
 
