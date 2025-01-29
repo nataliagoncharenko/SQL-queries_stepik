cf57a6e6c3ed00b2d634a274b0c12d3a1632ae96
nataliagoncharenko <mjiaahf@gmail.com>
2025-01-29 05:35:06 +0500

    Добавлен запрос

--- fine.sql
+++ fine.sql
@@ -9,3 +9,9 @@ CREATE TABLE fine(
     date_violation DATE,
     date_payment DATE)
    
+--2. В таблицу fine первые 5 строк уже занесены. Добавить в таблицу записи с ключевыми значениями 6, 7, 8.
+
+INSERT INTO fine (name, number_plate, violation, sum_fine, date_violation, date_payment)
+VALUES ('Баранов П.Е.', 'Р523ВТ', 'Превышение скорости(от 40 до 60)', null, '2020-02-14', null),
+('Абрамова К.А.', 'О111АВ', 'Проезд на запрещающий сигнал', null, '2020-02-23', null),
+('Яковлев Г.Р.', 'Т330ТТ', 'Проезд на запрещающий сигнал', null, '2020-03-03', null)
