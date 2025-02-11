6c336d593da684cd3c0da9824813b1fa552358fd
nataliagoncharenko <mjiaahf@gmail.com>
2025-02-11 19:32:12 +0500

    Новый запрос

--- fine.sql
+++ fine.sql
@@ -15,3 +15,9 @@ INSERT INTO fine (name, number_plate, violation, sum_fine, date_violation, date_
 VALUES ('Баранов П.Е.', 'Р523ВТ', 'Превышение скорости(от 40 до 60)', null, '2020-02-14', null),
 ('Абрамова К.А.', 'О111АВ', 'Проезд на запрещающий сигнал', null, '2020-02-23', null),
 ('Яковлев Г.Р.', 'Т330ТТ', 'Проезд на запрещающий сигнал', null, '2020-03-03', null)
+
+--3. Занести в таблицу fine суммы штрафов, которые должен оплатить водитель, в соответствии с данными из таблицы traffic_violation.
+
+UPDATE fine f, traffic_violation tv
+SET f.sum_fine = tv.sum_fine
+WHERE f.sum_fine IS NULL AND f.violation = tv.violation;
